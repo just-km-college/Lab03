58eab9c2245d27abbfe63fa1afc8429beb93f46b
karol <karol.majda@student.pk.edu.pl>
2024-12-02 21:19:21 +0100

    Commit

--- src/test/java/org/example/ShapeDescriberTest.java
+++ src/test/java/org/example/ShapeDescriberTest.java
@@ -13,17 +13,17 @@ public class ShapeDescriberTest {
 
         Color color = new Color(255,0,0);
 
-        Rectangle my_rect1 = new Rectangle(color,20,4);
-        Rectangle my_rect2 = new Rectangle();
-        Triangle my_triangle1 = new Triangle(color,6);
+        Rectangle rectangle1 = new Rectangle(color,20,4);
+        Rectangle rectangle2 = new Rectangle();
+        Triangle triangle = new Triangle(color,6);
 
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         PrintStream originalOut = System.out;
         System.setOut(new PrintStream(outputStream));
 
-        ShapeDescriber.describe(my_rect1);
-        ShapeDescriber.describe(my_rect2);
-        ShapeDescriber.describe(my_triangle1);
+        ShapeDescriber.describe(rectangle1);
+        ShapeDescriber.describe(rectangle2);
+        ShapeDescriber.describe(triangle);
 
         String expectedOut =
                 """
@@ -41,7 +41,7 @@ public class ShapeDescriberTest {
                         Area 15,59
                         Perimeter 18,00
                         Color Color[alfa=0, red=255, green=0, blue=0]
-                        
+
                         """;
 
         assertEquals(expectedOut,outputStream.toString());
